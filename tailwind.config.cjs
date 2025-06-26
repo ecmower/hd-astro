63f4a8bfdfd522d73ae56cf37f4c7197e83948ec
Elliott Mower <elliottmower@Elliotts-MacBook-Pro.local>
2025-06-26 18:11:04 -0400

    Fix GitHub Pages: restore base URL and use official Astro action

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,8 @@
 module.exports = {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./src/pages/**/*.astro",
+    "./src/components/**/*.astro"
   ],
   theme: {
     extend: {},
