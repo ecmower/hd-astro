23dc6f85378f411e88d4a014c4bafa4a4d84844d
Elliott Mower <elliottmower@Elliotts-MacBook-Pro.local>
2025-06-27 08:48:43 -0400

    fix: set base URL for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  base: '/hd-astro/'
+  base: '/hd-astro/',
+  integrations: [tailwind()],
 });
